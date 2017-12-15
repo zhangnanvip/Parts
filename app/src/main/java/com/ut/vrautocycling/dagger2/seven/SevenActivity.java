fdd73ea092974b96da76b23f0e94c58d344c8e7e
zhangnan <446816087@qq.com>
2017-12-15 14:30:42 +0800

    仿QQ消息数量提示使用

--- app/src/main/java/com/ut/vrautocycling/dagger2/seven/SevenActivity.java
+++ app/src/main/java/com/ut/vrautocycling/dagger2/seven/SevenActivity.java
@@ -2,11 +2,10 @@ package com.ut.vrautocycling.dagger2.seven;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.util.Log;
 
 import com.ut.vrautocycling.dagger2.R;
+import com.ut.vrautocycling.dagger2.seven.dragbubbleview.DragBubbleView;
 
 /**
  * 自定义小型组件测试
@@ -15,34 +14,31 @@ import com.ut.vrautocycling.dagger2.R;
  */
 public class SevenActivity extends AppCompatActivity {
 
-    private ViewGroup mVGRoot;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seven);
-        mVGRoot = findViewById(R.id.layout_root);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(32, 32);
-        params.setMargins(100, 0, 0, 0);
-        params.gravity = Gravity.CENTER;
+        DragBubbleView dbvNum = findViewById(R.id.dbv_num);
+        dbvNum.setOnBubbleStateListener(new DragBubbleView.OnBubbleStateListener() {
+            @Override
+            public void onDrag() {
+                Log.e("---> ", "拖拽气泡");
+            }
 
-        CircleTextView circleTextView = new CircleTextView(this);
-        circleTextView.setLayoutParams(params);
-        circleTextView.setBGColor(R.color.colorAccent);
-        circleTextView.setText("1");
-        circleTextView.setTextColor(R.color.colorPrimaryDark);
-        circleTextView.setTextSize(10);
-        circleTextView.setBorderColor(R.color.black);
-        mVGRoot.addView(circleTextView);
-        params.gravity = Gravity.CENTER;
+            @Override
+            public void onMove() {
+                Log.e("---> ", "移动气泡");
+            }
 
-        CircleTextView circleTextView2 = new CircleTextView(this);
-        circleTextView2.setLayoutParams(params);
-        circleTextView2.setBGColor(R.color.colorPrimaryDark);
-        circleTextView2.setText("2");
-        circleTextView2.setTextColor(R.color.colorAccent);
-        circleTextView2.setTextSize(10);
-        circleTextView2.setBorderColor(R.color.black);
-        mVGRoot.addView(circleTextView2);
+            @Override
+            public void onRestore() {
+                Log.e("---> ", "气泡恢复原来位置");
+            }
+
+            @Override
+            public void onDismiss() {
+                Log.e("---> ", "气泡消失");
+            }
+        });
     }
 }
