401e29f184357be3124c9c3edc981ef7d4392dd7
zhangnan <446816087@qq.com>
2018-01-02 17:46:08 +0800

    eight:矢量图动画使用

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 5. five：AutoCompleteTextView模拟网络模糊查询匹配
 6. six：ViewStub 简单使用
 7. seven：自定义小型组件测试 [DragBubbleView（仿QQ消息数量控件）,GarbageCanView（垃圾桶扔垃圾控件）]
+8. eight: SVG 矢量图动画使用 
 
 * layout_point_nine_drawable.xml：.9.png简单使用
 
---
+++ app/src/main/java/com/ut/vrautocycling/dagger2/eight/EightActivity.java
@@ -0,0 +1,54 @@
+package com.ut.vrautocycling.dagger2.eight;
+
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.jaredrummler.android.widget.AnimatedSvgView;
+import com.ut.vrautocycling.dagger2.R;
+
+/**
+ * SVG 矢量图动画实现方式
+ *
+ * @author zhangnan
+ * @date 2018/1/2
+ */
+public class EightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eight);
+        /*
+         * 方案一:
+         * 在 https://shapeshifter.design/ 制作矢量图动画
+         * 然后导出为 Animated Vector Drawable 的 xml 矢量图文件
+         * 直接放入 drawable 文件夹使用。
+         * 注意: (设置图片时使用 src ,不要使用 background)
+         */
+        ImageView ivSvg = findViewById(R.id.iv_animate);
+        Drawable drawable = ivSvg.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+
+        /*
+         * 方案二:
+         * 在模块 gradle 文件添加 AnimatedSvgView 依赖
+         * 在 http://www.iconfont.cn/collections 阿里巴巴素材库选择需要的矢量图以 SVG 格式下载
+         * Copy 出下载文件中的 pathData 、 fillColor 、viewportWidth 、 viewportHeight
+         * 为 AnimatedSvgView 设置属性。
+         */
+        final AnimatedSvgView svgView = findViewById(R.id.asv_animate);
+        svgView.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                svgView.start();
+            }
+        }, 500);
+    }
+
+}
